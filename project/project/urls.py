68283a36a61afbbff79d86f021b163b63b636f96
Matthew R. Whitt <mwhitt.w@gmail.com>
2018-05-12 12:49:43 -0700

    add dyno and gunicorn

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('main/', include('main.urls')),
+        *staticfiles_urlpatterns(),
         ]
